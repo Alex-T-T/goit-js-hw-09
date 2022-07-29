74a96814d14e33bd5b488892d3fc310969d34bc5
Olexandr Tolochko <0504fusion@gmail.com>
2022-07-29 15:06:36 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const body = document.querySelector('body');
+const buttons = document.querySelectorAll('button');
+const btnStart = buttons[0];
+const btnEnd = buttons[1];
+let timerId;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+btnStart.addEventListener('click', startOfGhangeColor);
+btnEnd.addEventListener('click', endOfChangeColor);
+
+
+function startOfGhangeColor() {
+
+    timerId = setInterval(() => {
+        const color = getRandomHexColor();
+        // console.log(color);
+        body.style.backgroundColor = color;
+        btnStart.removeEventListener('click', startOfGhangeColor);
+    }
+        , 1000);
+
+} 
+
+function endOfChangeColor() {
+    clearInterval(timerId);
+    btnStart.addEventListener('click', startOfGhangeColor);
+}
